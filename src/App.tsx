c352380bb9748c4bc021ee37bdc03b7f680e0834
braian <brasanluc123@gmail.com>
2023-06-08 10:12:52 -0300

    PeticionesAxios

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@
 // import { Funciones } from "./typescript/Funciones";
 // import { Contador } from "./components/Contador";
 // import { ContadorConHook } from "./components/ContadorConHook";
+// import { Login } from "./components/Login";
+import { Usuarios } from './components/Usuarios';
 
-import { Login } from "./components/Login";
 
 
 
@@ -18,7 +19,8 @@ const App = () => {
             {/* <Funciones /> */}
             {/* <Contador /> */}
             {/* <ContadorConHook /> */}
-            <Login />
+            {/* <Login /> */}
+            <Usuarios />
         </div>
     );
 };
---
+++ src/components/Usuarios.tsx
@@ -0,0 +1,18 @@
+
+export const Usuarios = () => {
+    return (
+        <>
+            <h3>Usuarios:</h3>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Avatar</th>
+                        <th>Nombre </th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+            </table>
+        </>
+    )
+}
