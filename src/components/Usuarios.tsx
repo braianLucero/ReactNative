1c4757fa611b16869dc01b9e674cf44f173c8c40
braian <brasanluc123@gmail.com>
2023-06-08 12:50:56 -0300

    exortandoJSON

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,10 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { reqResApi } from '../api/reqRes';
+import { ReqResListado, Usuario } from '../interfaces/reqRes';
 
 export const Usuarios = () => {
 
+    let api = reqResApi; // lo adapto a mi gusto 
+
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
     useEffect(() => {
-        reqResApi.get('/users')
+
+        api.get<ReqResListado>('/users')
             .then(res => {
                 console.log(res.data.data);
             })
